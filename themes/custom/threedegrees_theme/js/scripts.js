ce1805b0a51dd150f0401e97e465b21d21b6a14d
Amelia Schmidt <amelia@godeldesign.com>
2012-10-24 17:23:40 +1100

    added hook block and sprites

--- themes/custom/threedegrees_theme/js/scripts.js
+++ themes/custom/threedegrees_theme/js/scripts.js
@@ -1,9 +1,26 @@
 (function($){
   /* $ is mapped to Drupal's version of jQuery in here */
-  
+
   $(function(){
     /* document is ready in here */
-        
+
+/*  CarouFredSel: a circular, responsive jQuery carousel.
+  Configuration created by the "Configuration Robot"
+  at caroufredsel.frebsite.nl
+*/
+$(".views-field-field-slideshow-images ul").carouFredSel({
+  width: 900,
+  align: false,
+  height: 370,
+  items: {
+    visible: 1,
+    width: 900,
+    height: 370
+  },
+  pagination: "div.pagination",
+  scroll: 1000
+});
+
   });
-  
+
 })(jQuery);
